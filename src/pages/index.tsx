642ceef6b57a32e072c0e5aa62126f0c7c8cb987
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-08-29 14:18:53 +0800

    add google auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,25 @@
 import type { NextPage } from "next";
-import { trpc } from "../utils/trpc";
+// import { trpc } from "../utils/trpc";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 const Home: NextPage = () => {
-  const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
+  // const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
 
-  return <>Hello World</>;
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user?.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    );
+  }
+  return (
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
+  );
 };
 
 export default Home;
