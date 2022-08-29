1d4975da1dc067adcff97fe09dd456e27f3bd31a
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-08-29 10:34:13 +0800

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from "next";
+import { trpc } from "../utils/trpc";
+
+const Home: NextPage = () => {
+  const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
+
+  return <>Hello World</>;
+};
+
+export default Home;
