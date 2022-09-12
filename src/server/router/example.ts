1ab06682f0169cd9768141b3c2d219fd223799f7
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-09-12 13:52:36 +0800

    fix

--- src/server/router/example.ts
+++ src/server/router/example.ts
@@ -18,6 +18,7 @@ export const exampleRouter = createRouter()
   })
   .query("getAll", {
     async resolve({ ctx }) {
-      return await ctx.prisma.example.findMany();
+      // return await ctx.prisma.example.findMany();
+      return;
     },
   });
