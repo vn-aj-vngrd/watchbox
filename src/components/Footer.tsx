fae80e98b17a12ccb0b95b7a23da8c168062274c
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-09-13 17:42:15 +0800

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,8 @@
 
 const Footer = () => {
   return (
-    <footer className="flex justify-center p-2 px-6 py-2 text-sm border-t bg-white footer bg-neutral text-neutral-content dark:border-[#525252] dark:bg-black">
-      <div>
+    <footer className="p-2 px-6 py-2 text-sm border-t bg-white dark:border-[#525252] dark:bg-black">
+      <div className="container flex justify-center mx-auto">
         <p>© {new Date().getFullYear()} | NextDevs</p>
       </div>
     </footer>
