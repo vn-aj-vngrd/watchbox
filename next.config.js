fd87458a3bfd9ae01701fe803340ce6d1796e6fb
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-09-14 10:42:54 +0800

    SPRINT 2 - PWA

---
+++ next.config.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
+module.exports = withPWA({
+  // next.js config
+  reactStrictMode: true,
+  swcMinify: true,
+});
