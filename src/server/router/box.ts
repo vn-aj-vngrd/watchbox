324fa86df011f6613c63b70cb66fe5bedf82d57c
Nikolai Franz <franztumapon13@gmail.com>
2022-10-06 23:31:15 +0800

    Pagination, Sort, Search

--- src/server/router/box.ts
+++ src/server/router/box.ts
@@ -3,44 +3,107 @@ import { z } from "zod";
 
 export const boxRouter = createProtectedRouter()
   .query("getBoxes", {
-    input: z
-      .object({
-        skip: z.number(),
-        take: z.number(),
-      })
-      .nullish(),
+    input: z.object({
+      skip: z.number(),
+      take: z.number(),
+      searchParam: z.string().nullish(),
+      sortParam: z.string(),
+    }),
     async resolve({ input, ctx }) {
-      return ctx.prisma.box.findMany({
-        skip: input?.skip,
-        take: input?.take,
+      switch (input.sortParam) {
+        case "Newest":
+          return ctx.prisma.box.findMany({
+            skip: input?.skip,
+            take: input?.take,
+            where: {
+              userId: ctx.session.user.id,
+              boxTitle: {
+                contains: input.searchParam || undefined,
+                mode: "insensitive",
+              },
+            },
+            orderBy: {
+              created_at: "desc",
+            },
+          });
+        case "Oldest":
+          return ctx.prisma.box.findMany({
+            skip: input?.skip,
+            take: input?.take,
+            where: {
+              userId: ctx.session.user.id,
+              boxTitle: {
+                contains: input.searchParam || undefined,
+                mode: "insensitive",
+              },
+            },
+            orderBy: {
+              created_at: "asc",
+            },
+          });
+        case "A-Z":
+          return ctx.prisma.box.findMany({
+            skip: input?.skip,
+            take: input?.take,
+            where: {
+              userId: ctx.session.user.id,
+              boxTitle: {
+                contains: input.searchParam || undefined,
+                mode: "insensitive",
+              },
+            },
+            orderBy: {
+              boxTitle: "asc",
+            },
+          });
+        case "Z-A":
+          return ctx.prisma.box.findMany({
+            skip: input?.skip,
+            take: input?.take,
+            where: {
+              userId: ctx.session.user.id,
+              boxTitle: {
+                contains: input.searchParam || undefined,
+                mode: "insensitive",
+              },
+            },
+            orderBy: {
+              boxTitle: "desc",
+            },
+          });
+        default:
+          return ctx.prisma.box.findMany({
+            skip: input?.skip,
+            take: input?.take,
+            where: {
+              userId: ctx.session.user.id,
+            },
+            orderBy: {
+              created_at: "desc",
+            },
+          });
+      }
+    },
+  })
+  .query("getPageCount", {
+    async resolve({ ctx }) {
+      return ctx.prisma.box.count({
         where: {
           userId: ctx.session.user.id,
         },
       });
     },
   })
-
-  .query("getPageCount", {
-    async resolve({ctx}) {
-      return ctx.prisma.box.count({
-        where:{
+  .mutation("createBox", {
+    input: z.object({
+      boxTitle: z.string(),
+    }),
+    async resolve({ input, ctx }) {
+      return ctx.prisma.box.create({
+        data: {
           userId: ctx.session.user.id,
+          boxTitle: input.boxTitle,
         },
-      })
+      });
     },
-  })
-  // .mutation("createBox", {
-  //   input: z.object({
-  //     boxTitle: z.string(),
-  //     boxLink: z.string(),
-  //   }),
-  //   async resolve({ input, ctx }) {
-  //     return ctx.prisma.box.create({
-  //       data: {
-  //         userId: ctx.session.user.id,
-  //         boxTitle: input.boxTitle,
-  //         boxLink: input.boxLink,
-  //       },
-  //     });
-  //   },
-  // });
+  });
