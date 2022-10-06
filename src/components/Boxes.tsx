63c79587fb28e92f523be40c80b6166e309db407
Nikolai Franz <franztumapon13@gmail.com>
2022-10-06 17:38:29 +0800

    pagination

--- src/components/Boxes.tsx
+++ src/components/Boxes.tsx
@@ -1,32 +1,24 @@
 // components/Boxes.tsx
 
 type Props = {
-  boxes: {
+  boxes?: {
     boxTitle: string;
-    boxEntriesCount: number;
   }[];
-  pageIndex: number;
-  setPageCount: (pageCount: number) => void;
-
 };
 
-const Boxes: React.FC<Props> = ({ boxes, pageIndex, setPageCount }) => {
+const Boxes: React.FC<Props> = ({ boxes }) => {
 
-  // if boxes.length is lesser than 22, set page count to 1. if it is lesser than 44, set page count to 2, etc.
-  () => setPageCount(Math.ceil(boxes.length / 22));
-
-    
   return (
     <div className="grid md:w-full w-[80%] gap-x-14 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-7 gap-y-6 md:gap-y-8 mx-auto">
-      {boxes?.slice(21 * (pageIndex - 1), 21 * (pageIndex - 1) + 21).map((box, index) => (
+      {boxes?.map((box, index) => (
         <button key={index} className="flex flex-col items-center group">
-          <div className={`grid ${box.boxEntriesCount > 1 ? "grid-cols-2 grid-rows-2" : "grid-cols-1"} gap-3 w-32 p-4 transition duration-150 ease-in-out bg-blue-600 rounded-lg shadow-sm lg:w-36 aspect-square hover:scale-105`}>
+          {/* <div className={`grid ${box.boxEntriesCount > 1 ? "grid-cols-2 grid-rows-2" : "grid-cols-1"} gap-3 w-32 p-4 transition duration-150 ease-in-out bg-blue-200 rounded-lg shadow-sm lg:w-36 aspect-square hover:scale-105`}>
             
             {Array.from(Array(box.boxEntriesCount).keys()).filter((item, count) => count < 4).map(entry => 
               <div key={entry} className="bg-white rounded-lg"></div>
             )}
 
-          </div>
+          </div> */}
           <div className="p-2 text-center bg-transparent">
             <p className="subpixel-antialiased font-normal text-gray-600 dark:text-white">
               {box?.boxTitle}
--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -7,41 +7,47 @@ import {
 import { useEffect, useState } from "react";
 import Boxes from "./Boxes";
 import Favorites from "./Favorites";
-import Pagination from "./Pagination";
+import ReactPaginate from 'react-paginate';
+import { trpc } from "../utils/trpc";
 
 // XXX: temporary data; change this to db api call or whatever
-const boxList = [
-  { boxTitle: "Watched1", boxEntriesCount: 13 },
-  { boxTitle: "Watching2", boxEntriesCount: 2 },
-  { boxTitle: "On Hold3", boxEntriesCount: 1 },
-  { boxTitle: "Planned4", boxEntriesCount: 3 },
-  { boxTitle: "Dropped5", boxEntriesCount: 6 },
-  { boxTitle: "Watched6", boxEntriesCount: 13 },
-  { boxTitle: "Watching7", boxEntriesCount: 2 },
-  { boxTitle: "On Hold8", boxEntriesCount: 1 },
-  { boxTitle: "Planned9", boxEntriesCount: 3 },
-  { boxTitle: "Dropped10", boxEntriesCount: 6 },
-  { boxTitle: "Watched11", boxEntriesCount: 13 },
-  { boxTitle: "Watching12", boxEntriesCount: 2 },
-  { boxTitle: "On Hold13", boxEntriesCount: 1 },
-  { boxTitle: "Planned14", boxEntriesCount: 3 },
-  { boxTitle: "Dropped15", boxEntriesCount: 6 },
-  { boxTitle: "Watched16", boxEntriesCount: 13 },
-  { boxTitle: "Watching17", boxEntriesCount: 2 },
-  { boxTitle: "On Hold18", boxEntriesCount: 1 },
-  { boxTitle: "Planned19", boxEntriesCount: 3 },
-  { boxTitle: "Dropped20", boxEntriesCount: 6 },
-  { boxTitle: "Watched21", boxEntriesCount: 13 },
-  { boxTitle: "Watching22", boxEntriesCount: 2 },
-  { boxTitle: "On Hold23", boxEntriesCount: 1 },
-  { boxTitle: "Planned24", boxEntriesCount: 3 },
-  { boxTitle: "Dropped25", boxEntriesCount: 6 },
-  { boxTitle: "Watched26", boxEntriesCount: 13 },
-  { boxTitle: "Watching27", boxEntriesCount: 2 },
-  { boxTitle: "On Hold28", boxEntriesCount: 1 },
-  { boxTitle: "Planned29", boxEntriesCount: 3 },
-  { boxTitle: "Dropped30", boxEntriesCount: 6 },
-];
+
+type BoxList = {
+    boxTitle: string;
+}
+
+// const boxList = [
+//   { boxTitle: "Watched1", boxEntriesCount: 13 },
+//   { boxTitle: "Watching2", boxEntriesCount: 2 },
+//   { boxTitle: "On Hold3", boxEntriesCount: 1 },
+//   { boxTitle: "Planned4", boxEntriesCount: 3 },
+//   { boxTitle: "Dropped5", boxEntriesCount: 6 },
+//   { boxTitle: "Watched6", boxEntriesCount: 13 },
+//   { boxTitle: "Watching7", boxEntriesCount: 2 },
+//   { boxTitle: "On Hold8", boxEntriesCount: 1 },
+//   { boxTitle: "Planned9", boxEntriesCount: 3 },
+//   { boxTitle: "Dropped10", boxEntriesCount: 6 },
+//   { boxTitle: "Watched11", boxEntriesCount: 13 },
+//   { boxTitle: "Watching12", boxEntriesCount: 2 },
+//   { boxTitle: "On Hold13", boxEntriesCount: 1 },
+//   { boxTitle: "Planned14", boxEntriesCount: 3 },
+//   { boxTitle: "Dropped15", boxEntriesCount: 6 },
+//   { boxTitle: "Watched16", boxEntriesCount: 13 },
+//   { boxTitle: "Watching17", boxEntriesCount: 2 },
+//   { boxTitle: "On Hold18", boxEntriesCount: 1 },
+//   { boxTitle: "Planned19", boxEntriesCount: 3 },
+//   { boxTitle: "Dropped20", boxEntriesCount: 6 },
+//   { boxTitle: "Watched21", boxEntriesCount: 13 },
+//   { boxTitle: "Watching22", boxEntriesCount: 2 },
+//   { boxTitle: "On Hold23", boxEntriesCount: 1 },
+//   { boxTitle: "Planned24", boxEntriesCount: 3 },
+//   { boxTitle: "Dropped25", boxEntriesCount: 6 },
+//   { boxTitle: "Watched26", boxEntriesCount: 13 },
+//   { boxTitle: "Watching27", boxEntriesCount: 2 },
+//   { boxTitle: "On Hold28", boxEntriesCount: 1 },
+//   { boxTitle: "Planned29", boxEntriesCount: 3 },
+//   { boxTitle: "Dropped30", boxEntriesCount: 6 },
+// ];
 
 const favList = [
   { favoriteTitle: "My Fav 1", favoriteEntriesCount: 13 },
@@ -58,25 +64,34 @@ const sortOptions = [
   { id: "four", name: "Z-A" },
 ];
 
+const itemsPerPage = 14;
+
 const Dashboard = () => {
+  const [currentItems, setCurrentItems] = useState<BoxList[]>([]);
   const [collection, setCollection] = useState("boxes");
-  const [boxes] = useState(boxList);
-  const [faves] = useState(favList);
   const [openSort, setOpenSort] = useState<boolean>(false);
   const [sortArr, setSortArr] = useState<boolean[]>([]);
-  const [pageIndex, setPageIndex] = useState(1);
-  const [pageCount, setPageCount] = useState(1);
+  const [paginationNumber, setpaginationNumber] = useState(0);
 
-  useEffect(() => {
-    setSortArr(Array(sortOptions.length).fill(false));
-  }, []);
+  const boxesData = trpc.useQuery(["box.getBoxes", {skip:paginationNumber, take:itemsPerPage}]);
+  const boxesCount = trpc.useQuery(["box.getPageCount"]);
 
-  const onSort = (id: number) => {
-    const newSortOptions = Array(sortOptions.length).fill(false);
-    newSortOptions[id] = !newSortOptions[id];
-    setSortArr(newSortOptions);
+  console.log(boxesData.data);
+  
+  // useEffect(() => {
+  //    setSortArr(Array(sortOptions.length).fill(false));
+  //    setCurrentItems(boxList);
+  //   }, [boxList]);
+
+   const onSort = (id: number) => {
+     const newSortOptions = Array(sortOptions.length).fill(false);
+     newSortOptions[id] = !newSortOptions[id];
+     setSortArr(newSortOptions);
+   };
+
+  const handlePageClick = (event:{selected:number}) => {
+    setpaginationNumber(event.selected);
   };
-
   
   return (
     <div className="w-full py-6 space-y-8">
@@ -85,7 +100,7 @@ const Dashboard = () => {
           <button 
             onClick={() => setCollection("boxes")} 
             className={`text-2xl subpixel-antialiased hover:text-blue-600 ${collection === "boxes" ? "font-medium" : "font-normal"}`}>
-            Boxes {pageIndex}
+            Boxes 
           </button>
           <button 
             onClick={() => setCollection("favorites")} 
@@ -158,11 +173,55 @@ const Dashboard = () => {
 
       {
         collection === "boxes" ? 
-          <Boxes pageIndex={pageIndex} boxes={boxes} setPageCount={setPageCount} /> : 
-          <Favorites pageIndex={pageIndex} favorites={faves} />
+          <Boxes boxes={boxesData.data} /> : <></>
+          // <Favorites favorites={faves} />
       }
 
-      <Pagination pageCount={pageCount} pageIndex={pageIndex} setPageIndex={setPageIndex}  />
+        <div className="flex justify-center">
+            <ReactPaginate
+              breakLabel="..."
+              onPageChange={handlePageClick}
+              pageRangeDisplayed={itemsPerPage}
+              pageCount={boxesCount?.data !== undefined ? Math.ceil(boxesCount.data/itemsPerPage) : 0}
+              previousLabel={
+                <svg
+                  aria-hidden="true"
+                  className="w-6 h-6"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              }
+              nextLabel={
+                <svg
+                  aria-hidden="true"
+                  className="w-6 h-6"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              }
+              previousLinkClassName="block duration-300 ease-in-out py-2 px-3 text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
+              nextLinkClassName={
+                "block py-2 px-3 duration-300 ease-in-out text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
+              }
+              className="flex space-x-2"
+              pageLinkClassName="block py-2 px-3 text-gray-500 bg-white border border-gray-300 duration-300 ease-in-out hover:bg-gray-100 hover:text-gray-700"
+              activeLinkClassName="bg-[#6A74CF] border-[#6A74CF] text-gray-50 font-bold duration-300 ease-in-out hover:bg-primaryColor hover:text-gray-50 hover:border-[#6A74CF]"
+            />
+          </div>
 
     </div>
   );
