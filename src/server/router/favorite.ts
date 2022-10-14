bf5fd80ee8f7d94fe8d1ba2579ca619e2f4b7561
Nikolai Franz <franztumapon13@gmail.com>
2022-10-14 12:21:34 +0800

    Box responsiveness | Image Component | A-Z, Z-A Sort

--- src/server/router/favorite.ts
+++ src/server/router/favorite.ts
@@ -1,6 +1,5 @@
 import { createProtectedRouter } from "./protected-router";
 import { z } from "zod";
-import { Box } from "@prisma/client";
 
 export const favoriteRouter = createProtectedRouter()
   .query("getFavorites", {
@@ -11,48 +10,29 @@ export const favoriteRouter = createProtectedRouter()
       sortParam: z.string(),
     }),
     async resolve({ input, ctx }) {
-      const userId = ctx.session.user.id;
-
-      switch (input.sortParam) {
-        case "Newest":
-          return ctx.prisma.$queryRaw<Box[]>`SELECT *
-              FROM "public"."FavoriteBox" 
-              LEFT JOIN "public"."Box" ON "public"."Box"."id" = "public"."FavoriteBox"."boxId"
-              LEFT JOIN "public"."Entry" on "public"."Entry"."boxId" = "public"."Box"."id"
-              WHERE "public"."FavoriteBox"."userId" = ${userId}
-              ORDER BY "public"."FavoriteBox"."created_at" desc
-              LIMIT ${input.take} OFFSET ${input.skip}`;
-
-        case "Oldest":
-          return ctx.prisma.$queryRaw<Box[]>`SELECT *
-              FROM "public"."FavoriteBox" 
-              LEFT JOIN "public"."Box" ON "public"."Box"."id" = "public"."FavoriteBox"."boxId"
-              LEFT JOIN "public"."Entry" on "public"."Entry"."boxId" = "public"."Box"."id"
-              WHERE "public"."FavoriteBox"."userId" = ${userId}
-              ORDER BY "public"."FavoriteBox"."created_at" asc
-              LIMIT ${input.take} OFFSET ${input.skip}`;
-
-        case "A-Z":
-          return ctx.prisma.$queryRaw<Box[]>`SELECT *
-              FROM "public"."FavoriteBox" 
-              LEFT JOIN "public"."Box" ON "public"."Box"."id" = "public"."FavoriteBox"."boxId"
-              LEFT JOIN "public"."Entry" on "public"."Entry"."boxId" = "public"."Box"."id"
-              WHERE "public"."FavoriteBox"."userId" = ${userId}
-              ORDER BY "public"."Box"."boxTitle" asc
-              LIMIT ${input.take} OFFSET ${input.skip}`;
-
-        case "Z-A":
-          return ctx.prisma.$queryRaw<Box[]>`SELECT *
-              FROM "public"."FavoriteBox" 
-              LEFT JOIN "public"."Box" ON "public"."Box"."id" = "public"."FavoriteBox"."boxId"
-              LEFT JOIN "public"."Entry" on "public"."Entry"."boxId" = "public"."Box"."id"
-              WHERE "public"."FavoriteBox"."userId" = ${userId}
-              ORDER BY "public"."Box"."boxTitle" desc
-              LIMIT ${input.take} OFFSET ${input.skip}`;
-
-        default:
-          return;
-      }
+      return ctx.prisma.box.findMany({
+        take: input.take,
+        skip: input.skip,
+        where: {
+          FavoriteBox: {
+            some: {
+              userId: ctx.session.user.id,
+            },
+          },
+        },
+        include: {
+          Entry: true,
+          FavoriteBox: true,
+        },
+        orderBy: {
+          boxTitle:
+            input.sortParam === "Z-A"
+              ? "desc"
+              : input.sortParam === "A-Z"
+              ? "asc"
+              : undefined,
+        },
+      });
     },
   })
   .query("getFavoritesCount", {
