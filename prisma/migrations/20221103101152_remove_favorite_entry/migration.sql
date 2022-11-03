42fa03441a75a10826ba783102dbb89e0bdf7b2d
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-11-03 18:13:37 +0800

    update database schema

---
+++ prisma/migrations/20221103101152_remove_favorite_entry/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the `FavoriteEntry` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "FavoriteEntry";
