747d373a636300933d9a8621fadb8c1fffb9bd19
teddzyb <teddybartletg88@gmail.com>
2022-11-04 22:25:20 +0800

    Update home icon to use 20x20 style

--- src/components/Common/Header.tsx
+++ src/components/Common/Header.tsx
@@ -1,6 +1,6 @@
 // components/Header.tsx
 
-import { HomeIcon, SquaresPlusIcon } from "@heroicons/react/24/solid";
+import { HomeIcon, SquaresPlusIcon } from "@heroicons/react/20/solid";
 import router from "next/router";
 import ToggleTheme from "./ToggleTheme";
 import AvatarDropdown from "../Account/AvatarDropdown";
@@ -12,19 +12,19 @@ type Props = {
 
 const Header: React.FC<Props> = ({ session }) => {
   return (
-    <header className="sticky top-0 z-10 py-3.5 bg-white dark:bg-darkerColor dark:border-darkerColor">
-      <div className="flex flex-wrap items-center justify-between px-4 mx-auto">
+    <header className="sticky top-0 z-10 bg-white py-3.5 dark:border-darkerColor dark:bg-darkerColor">
+      <div className="mx-auto flex flex-wrap items-center justify-between px-4">
         <div>
           <button onClick={() => router.push("/")} className="hidden md:flex">
             <h1 className="flex items-center text-xl font-semibold text-black dark:text-white">
-              <SquaresPlusIcon className="w-6 h-6 mr-3 text-blue-600" />
+              <SquaresPlusIcon className="mr-3 h-6 w-6 text-blue-600" />
               WatchBox
             </h1>
           </button>
 
           <button onClick={() => router.push("/")} className="flex md:hidden">
             <div className="flex items-center text-base font-semibold">
-              <SquaresPlusIcon className="w-6 h-6 mr-2 fill-blue-600" />
+              <SquaresPlusIcon className="mr-2 h-6 w-6 fill-blue-600" />
             </div>
           </button>
         </div>
@@ -37,9 +37,9 @@ const Header: React.FC<Props> = ({ session }) => {
                   <button
                     onClick={() => router.push("/")}
                     type="button"
-                    className="inline-flex items-center p-1 bg-white border border-gray-300 rounded-full shadow-sm hover:bg-gray-200 focus:outline-none dark:bg-darkColor dark:text-white dark:border-transparent dark:hover:bg-grayColor"
+                    className="inline-flex items-center rounded-full border border-gray-300 bg-white p-1 shadow-sm focus:outline-none hover:bg-gray-200 dark:border-transparent dark:bg-darkColor dark:text-white dark:hover:bg-grayColor"
                   >
-                    <HomeIcon className="w-5 h-5 fill-black dark:fill-white" />
+                    <HomeIcon className="h-5 w-5 fill-black dark:fill-white" />
                   </button>
                 </div>
 
