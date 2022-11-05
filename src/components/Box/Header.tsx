a5b7d7be8c676c2bce754d1f3464aa41a35aecb7
teddzyb <teddybartletg88@gmail.com>
2022-11-05 22:58:01 +0800

    Add header

--- src/components/Box/Header.tsx
+++ src/components/Box/Header.tsx
@@ -1,7 +1,33 @@
-// Imports
+import { HeartIcon as SolidHeartIcon, ShareIcon, Cog6ToothIcon } from "@heroicons/react/24/solid";
+import { HeartIcon as OutlineHeartIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
 
 const Header = () => {
-  return <div className="flex h-12 items-center border-b dark:border-darkColor">Header</div>;
+  const [favorite, setFavorite] = useState(false);
+
+  return (
+    <div className="flex h-12 items-center border-b px-4 dark:border-darkColor">
+      <div className="flex h-full grow items-center font-medium">Box Title</div>
+      <div className="flex h-full items-center">
+        <button
+          onClick={() => setFavorite(!favorite)}
+          className="flex h-full w-11 items-center justify-center"
+        >
+          {favorite ? (
+            <SolidHeartIcon className="h-5 w-5 text-red-500" />
+          ) : (
+            <OutlineHeartIcon className="h-5 w-5 dark:text-white" />
+          )}
+        </button>
+        <button className="flex h-full w-11 items-center justify-center">
+          <ShareIcon className="h-[18px] w-[18px] dark:text-white" />
+        </button>
+        <button className="flex h-full w-11 items-center justify-center">
+          <Cog6ToothIcon className="h-5 w-5 dark:text-white" />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
