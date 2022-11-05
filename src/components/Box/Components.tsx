4b37cb58bba03d026dd12997db23ce6ce158697b
teddzyb <teddybartletg88@gmail.com>
2022-11-06 00:22:07 +0800

    Add Entry component

--- src/components/Box/Components.tsx
+++ src/components/Box/Components.tsx
@@ -1,7 +1,29 @@
 // Imports
 
 const Components = () => {
-  return <div className="flex h-full justify-center">Components</div>;
+  return (
+    <div className="flex h-full justify-center scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-blue-500">
+      <div className="my-4 grid h-fit grid-cols-2 gap-4">
+        <div className="flex h-28 w-28 select-none items-center justify-center rounded-lg bg-gray-200 text-gray-700">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="36"
+            height="36"
+            preserveAspectRatio="xMidYMid meet"
+            viewBox="0 0 24 24"
+          >
+            <g fill="none">
+              <path d="M0 0h24v24H0z" />
+              <path
+                fill="currentColor"
+                d="M12 2c5.523 0 10 4.477 10 10a9.982 9.982 0 0 1-3.76 7.814l-.239.186H20a1 1 0 0 1 .117 1.993L20 22h-8C6.477 22 2 17.523 2 12S6.477 2 12 2Zm0 2a8 8 0 1 0 0 16a8 8 0 0 0 0-16Zm0 10a2 2 0 1 1 0 4a2 2 0 0 1 0-4Zm-4-4a2 2 0 1 1 0 4a2 2 0 0 1 0-4Zm8 0a2 2 0 1 1 0 4a2 2 0 0 1 0-4Zm-4-4a2 2 0 1 1 0 4a2 2 0 0 1 0-4Z"
+              />
+            </g>
+          </svg>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Components;
