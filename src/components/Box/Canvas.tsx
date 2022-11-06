15ca83a61512dde4c33b4be74105f94a3ff68491
teddzyb <teddybartletg88@gmail.com>
2022-11-06 12:19:17 +0800

    Optimize dark mode

--- src/components/Box/Canvas.tsx
+++ src/components/Box/Canvas.tsx
@@ -2,7 +2,7 @@
 
 const Canvas = () => {
   return (
-    <div className="flex h-full select-none items-center justify-center text-sm text-gray-500">
+    <div className="flex h-full select-none items-center justify-center text-sm text-gray-500 dark:text-neutral-400">
       Add your first entry
     </div>
   );
