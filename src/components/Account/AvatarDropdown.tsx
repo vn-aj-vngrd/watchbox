e67157276ccfb8afbe26aaa6929f8f1a75eac1e7
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-11-07 13:38:41 +0800

    fix avatar dropdown warning

--- src/components/Account/AvatarDropdown.tsx
+++ src/components/Account/AvatarDropdown.tsx
@@ -1,13 +1,13 @@
 // components/AvatarDropdown.tsx
 
-import { Fragment, useEffect, useState } from "react";
 import { Menu, Transition } from "@headlessui/react";
 import { ArrowLeftOnRectangleIcon, Cog8ToothIcon, UserIcon } from "@heroicons/react/24/solid";
-import Image from "next/image";
-import { signOut } from "next-auth/react";
-import Spinner from "../Common/Spinner";
-import router from "next/router";
 import { Session } from "next-auth";
+import { signOut } from "next-auth/react";
+import Image from "next/image";
+import router from "next/router";
+import { Fragment, useEffect, useState } from "react";
+import Spinner from "../Common/Spinner";
 
 type Props = {
   session: Session | null;
@@ -32,21 +32,19 @@ const AvatarDropdown: React.FC<Props> = ({ session }) => {
   return (
     <Menu as="div" className="relative inline-block text-left">
       <div>
-        <Menu.Button className="inline-flex items-center p-1 text-gray-900 bg-white border border-gray-300 rounded-full shadow-sm hover:bg-gray-200 focus:outline-none dark:bg-darkColor dark:text-white dark:border-transparent dark:hover:bg-grayColor">
-          <div className="w-5 h-5">
-            {session?.user?.image ? (
-              <Image
-                src={session?.user?.image || ""}
-                loader={({ src }) => `${src}?w=500&q=100`}
-                alt=""
-                layout="fill"
-                priority
-                className="rounded-full relative"
-              />
-            ) : (
-              <UserIcon />
-            )}
-          </div>
+        <Menu.Button className="relative mt-1 flex h-[30px] w-[30px] rounded-full border border-gray-300 bg-white text-gray-900 shadow-sm focus:outline-none hover:bg-gray-200 dark:border-transparent dark:bg-darkColor dark:text-white dark:hover:bg-grayColor">
+          {session?.user?.image ? (
+            <Image
+              src={session?.user?.image || ""}
+              loader={({ src }) => `${src}?w=500&q=100`}
+              alt=""
+              layout="fill"
+              priority
+              className="relative rounded-full"
+            />
+          ) : (
+            <UserIcon />
+          )}
         </Menu.Button>
       </div>
 
@@ -59,10 +57,10 @@ const AvatarDropdown: React.FC<Props> = ({ session }) => {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 w-56 mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none dark:bg-darkColor dark:divide-grayColor">
+        <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none dark:divide-grayColor dark:bg-darkColor">
           <div className="px-4 py-3">
             <p className="text-sm">Signed in as</p>
-            <p className="text-sm font-medium text-blue-500 truncate dark:text-blue-400">
+            <p className="truncate text-sm font-medium text-blue-500 dark:text-blue-400">
               {session?.user?.email}
             </p>
           </div>
@@ -70,9 +68,9 @@ const AvatarDropdown: React.FC<Props> = ({ session }) => {
             <Menu.Item>
               <button
                 onClick={() => router.push("/account")}
-                className="flex items-center w-full px-4 py-2 text-sm text-left text-gray-900 hover:bg-gray-100 hover:text-gray-900 dark:text-white dark:hover:bg-grayColor"
+                className="flex w-full items-center px-4 py-2 text-left text-sm text-gray-900 hover:bg-gray-100 hover:text-gray-900 dark:text-white dark:hover:bg-grayColor"
               >
-                <Cog8ToothIcon className="w-6 h-6 p-[0.1rem] mr-2 rounded-full dark:text-white" />
+                <Cog8ToothIcon className="mr-2 h-6 w-6 rounded-full p-[0.1rem] dark:text-white" />
                 Account Settings
               </button>
             </Menu.Item>
@@ -81,9 +79,9 @@ const AvatarDropdown: React.FC<Props> = ({ session }) => {
             <Menu.Item>
               <button
                 onClick={onClick}
-                className="flex items-center w-full px-4 py-2 text-sm text-left text-gray-900 hover:bg-gray-100 hover:text-gray-900 dark:text-white dark:hover:bg-grayColor"
+                className="flex w-full items-center px-4 py-2 text-left text-sm text-gray-900 hover:bg-gray-100 hover:text-gray-900 dark:text-white dark:hover:bg-grayColor"
               >
-                <ArrowLeftOnRectangleIcon className="w-6 h-6 p-[0.1rem] mr-2 rounded-full dark:text-white" />
+                <ArrowLeftOnRectangleIcon className="mr-2 h-6 w-6 rounded-full p-[0.1rem] dark:text-white" />
                 Sign Out
               </button>
             </Menu.Item>
