d738d43b717d59409cf0cc92cb55b01943d8c409
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-11-08 17:59:15 +0800

    internal server error fix

--- src/components/Box/BoxPage.tsx
+++ src/components/Box/BoxPage.tsx
@@ -1,26 +1,46 @@
-import { FavoriteBox } from "@prisma/client";
+import { XCircleIcon } from "@heroicons/react/24/solid";
+import { useRouter } from "next/router";
 import { useState } from "react";
+import { trpc } from "../../utils/trpc";
+import PageAlert from "../Common/PageAlert";
+import Spinner from "../Common/Spinner";
 import Canvas from "./Canvas";
 import Components from "./Components";
 import Controls from "./Controls";
 import Header from "./Header";
 
-type Props = {
-  box:
-    | {
-        id: string;
-        username: string | null;
-        boxes: { id: string; created_at: Date; updated_at: Date; boxTitle: string }[];
-      }
-    | null
-    | undefined;
-  favoriteBox: FavoriteBox | null | undefined;
-  id: string;
-  refetch: () => void;
-};
-
-const BoxPage = ({ box, favoriteBox, id, refetch }: Props) => {
+const BoxPage = () => {
   const [sidePanel, setSidePanel] = useState(true);
+
+  const router = useRouter();
+  const { id } = router.query;
+
+  const getBox = trpc.useQuery(["box.getBox", { id: id as string }]);
+  const getFavoriteBox = trpc.useQuery(["favorite.getFavoriteBox", { boxId: id as string }]);
+
+  if (getBox.isLoading || getFavoriteBox.isLoading) {
+    return <Spinner isGlobal={true} />;
+  }
+
+  if (getBox.isError || getFavoriteBox.isError) {
+    return (
+      <PageAlert
+        title="Something went wrong"
+        elem={
+          <h2 className="mb-10 flex justify-center">
+            <XCircleIcon className="h-12 w-12 text-red-500" />
+          </h2>
+        }
+        description={["There's a problem on our side. Please try again."]}
+        btnTitle="Go back to home"
+      />
+    );
+  }
+
+  const refetch = () => {
+    getBox.refetch();
+    getFavoriteBox.refetch();
+  };
 
   return (
     <div className="flex h-full w-full">
@@ -33,7 +53,12 @@ const BoxPage = ({ box, favoriteBox, id, refetch }: Props) => {
         <Components sidePanel={sidePanel} />
       </div>
       <div className="flex h-full grow flex-col">
-        <Header box={box} favoriteBox={favoriteBox} id={id} refetch={refetch} />
+        <Header
+          box={getBox?.data}
+          favoriteBox={getFavoriteBox?.data}
+          id={id as string}
+          refetch={refetch}
+        />
         <Canvas />
       </div>
     </div>
