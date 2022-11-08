8801ad608d6220029b46a7a6826cbb8d5813bd90
Van AJ Vanguardia <vanajvanguardia@gmail.com>
2022-11-08 20:37:47 +0800

    fix spinner color

--- src/components/Common/Spinner.tsx
+++ src/components/Common/Spinner.tsx
@@ -13,7 +13,7 @@ const Spinner = ({ isGlobal }: Props) => {
       >
         <svg
           aria-hidden="true"
-          className="h-8 w-8 animate-spin fill-blue-600 dark:text-gray-600"
+          className="h-8 w-8 animate-spin fill-blue-600 text-gray-200 dark:text-gray-600"
           viewBox="0 0 100 101"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
@@ -36,7 +36,7 @@ const Spinner = ({ isGlobal }: Props) => {
     <div className="text-center">
       <div role="status">
         <svg
-          className="inline h-8 w-8 animate-spin fill-blue-600 dark:text-gray-600"
+          className="inline h-8 w-8 animate-spin fill-blue-600 text-gray-200 dark:text-gray-600"
           viewBox="0 0 100 101"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
