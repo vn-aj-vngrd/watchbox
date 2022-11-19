0d5ccffc6bc74f0447d308120e4a86f268712b83
Nikolai Franz <franztumapon13@gmail.com>
2022-11-19 15:21:04 +0800

    EntryHeader and EntryPage partial

---
+++ src/components/Entry/EntryHeader.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { FavoriteBox } from "@prisma/client";
+import { trpc } from "../../utils/trpc";
+import { server } from "../../config";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+
+type Props = {
+  box:
+    | {
+        id: string;
+        username: string | null;
+        boxes: { id: string; created_at: Date; updated_at: Date; boxTitle: string }[];
+      }
+    | null
+    | undefined;
+  favoriteBox: FavoriteBox | null | undefined;
+  id: string;
+  refetch: () => void;
+};
+
+type Inputs = {
+  status: string;
+};
+
+const EntryHeader = ({ box, favoriteBox, id, refetch }: Props) => {
+  return <div className="flex h-12 items-center border-b pl-4 pr-2 dark:border-darkColor"></div>;
+};
+
+export default EntryHeader;
--- src/pages/box/entry.tsx
+++ src/pages/box/entry.tsx
@@ -1,12 +1,48 @@
 import { GetServerSidePropsContext } from "next";
 import Meta from "../../components/Common/Meta";
 import { getServerSideSession } from "../../utils/session";
+import EntryPage from "../../components/Entry/EntryPage";
+import Spinner from "../../components/Common/Spinner";
+import { trpc } from "../../utils/trpc";
+import PageAlert from "../../components/Common/PageAlert";
+import { XCircleIcon } from "@heroicons/react/24/solid";
 
-const entry = () => {
+type Props = {
+  id: string;
+};
+
+const entry = (/*{ id }: Props*/) => {
+  // const getBox = trpc.useQuery(["box.getBox", { id }]);
+  // const getFavoriteBox = trpc.useQuery(["favorite.getFavoriteBox", { boxId: id }]);
+
+  // if (getBox.isLoading || getFavoriteBox.isLoading) {
+  // return <Spinner isGlobal={true} />;
+  // }
+
+  // if (getBox.isError || getFavoriteBox.isError) {
+  // return (
+  //   <PageAlert
+  //     title="Something went wrong"
+  //     elem={
+  //       <h2 className="mb-10 flex justify-center">
+  //         <XCircleIcon className="h-12 w-12 text-red-500" />
+  //       </h2>
+  //     }
+  //     description={["There's a problem on our side. Please try again."]}
+  //     btnTitle="Go back to home"
+  //   />
+  // );
+  // }
+
+  // const refetch = () => {
+  //   getBox.refetch();
+  //   getFavoriteBox.refetch();
+  // };
+
   return (
     <>
       <Meta title="WatchBox | Entry" />
-      <p className="text-center">Entry</p>
+      <EntryPage />
     </>
   );
 };
