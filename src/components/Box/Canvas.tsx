bfeaa3ed1c12c825ef2d6a74f2ac2e5810e9e485
Heischichou <jangarot@gmail.com>
2022-11-26 14:45:01 +0800

    Update Movie Search Box

--- src/components/Box/Canvas.tsx
+++ src/components/Box/Canvas.tsx
@@ -23,6 +23,7 @@ type Movie = {
 const Canvas = () => {
   const [title, setTitle] = useState("");
   const [movies, setMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState<Movie>();
 
   const getPopularMovies = async () => {
     const req = await fetch(
@@ -35,6 +36,18 @@ const Canvas = () => {
     results = results.slice(0, 10);
     setMovies(results);
     setTitle(results[0].original_title);
+    setSelectedMovie(results[0].id);
+  };
+
+  const searchMovies = async () => {
+    const req = await fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=${env.NEXT_PUBLIC_TMDB_API_KEY}&language=en-US&query=${title}&page=1&include_adult=true`,
+      {
+        method: "GET",
+      },
+    ).then((res) => res.json());
+    const { results } = req;
+    setMovies(results);
   };
 
   useEffect(() => {
@@ -47,19 +60,24 @@ const Canvas = () => {
     title === ""
       ? movies.map((movie: Movie) => movie)
       : movies.filter((movie: Movie) => {
-          return movie.original_title.toLowerCase().includes(title.toLowerCase());
+          return movie.original_title.toLowerCase().includes(title.trim().toLowerCase());
         });
+
+  const handleInputChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+    title.length > 0 ? searchMovies() : getPopularMovies();
+  };
 
   return (
     <div>
       <div className="flex h-full select-none items-center justify-center text-sm text-gray-500 dark:text-neutral-400">
         Add your first entry
       </div>
-      <Combobox value={title} onChange={setTitle}>
-        <Combobox.Input onChange={(event) => setTitle(event.target.value)} />
+      <Combobox value={selectedMovie} onChange={setSelectedMovie}>
+        <Combobox.Input value={title} onChange={handleInputChange} />
         <Combobox.Options>
           {filteredMovies.map((movie: Movie) => (
-            <Combobox.Option key={movie.id} value={movie.original_title}>
+            <Combobox.Option key={movie.id} value={movie}>
               {movie.original_title}
             </Combobox.Option>
           ))}
