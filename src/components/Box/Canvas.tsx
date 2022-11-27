d82e423c4cb7fa8ef9667b59a89cef197836a1c9
teddzyb <teddybartletg88@gmail.com>
2022-11-28 00:10:43 +0800

    Fix linting issues

--- src/components/Box/Canvas.tsx
+++ src/components/Box/Canvas.tsx
@@ -17,24 +17,21 @@ const Canvas: React.FC<Props> = ({ canvasRef, canvasElements }) => {
     // TODO: add right and bottom padding to canvas
     <div
       ref={canvasRef}
-      className="relative flex h-full select-none items-center justify-center scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-blue-500">
-    {canvasElements.length === 0 ? (
+      className="relative flex h-full select-none items-center justify-center scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-blue-500"
+    >
+      {canvasElements.length === 0 ? (
         <span className="text-sm text-gray-500 dark:text-neutral-400">Add your first entry</span>
-    ) : (
+      ) : (
         canvasElements.map((canvasElement, index) => {
           switch (canvasElement.component) {
             case "text":
-              return (
-                <TextComponent key={index} canvasElement={canvasElement} />
-              );
+              return <TextComponent key={index} canvasElement={canvasElement} />;
             case "entry":
-              return (
-                <EntryComponent key={index} canvasElement={canvasElement} />
-              );
+              return <EntryComponent key={index} canvasElement={canvasElement} />;
           }
         })
       )}
-  </div>
+    </div>
   );
 };
 
--- src/components/Box/Components/TextComponent.tsx
+++ src/components/Box/Components/TextComponent.tsx
@@ -1,24 +1,22 @@
 type CanvasElement = {
-    component: string;
-    x: number;
-    y: number;
+  component: string;
+  x: number;
+  y: number;
 };
 
 type Props = {
-    canvasElement: CanvasElement;
-  };
+  canvasElement: CanvasElement;
+};
 
 const TextComponent = ({ canvasElement }: Props) => {
-
-    return (
+  return (
     <div
-        className="absolute flex h-20 w-72 items-center justify-center rounded-md bg-gray-200 text-sm dark:bg-darkColor"
-        style={{ top: canvasElement.y - 40, left: canvasElement.x - 144 }}>
-    <div className="flex h-full w-full items-center justify-center">
-        Text Component
-        </div>
+      className="absolute flex h-20 w-72 items-center justify-center rounded-md bg-gray-200 text-sm dark:bg-darkColor"
+      style={{ top: canvasElement.y - 40, left: canvasElement.x - 144 }}
+    >
+      <div className="flex h-full w-full items-center justify-center">Text Component</div>
     </div>
-    );
+  );
 };
 
 export default TextComponent;
