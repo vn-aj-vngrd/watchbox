2ed5afbb9f2f325eb7bf49536840359a0a13a243
teddzyb <teddybartletg88@gmail.com>
2022-12-11 18:27:52 +0800

    Update divider component icon

--- src/components/Box/Components.tsx
+++ src/components/Box/Components.tsx
@@ -225,6 +225,7 @@ const Components: React.FC<Props> = ({
           !sidePanel ? "md:my-3 md:grid-cols-1 md:gap-3" : "md:my-4 md:grid-cols-2 md:gap-4"
         }`}
       >
+        {/* Text Component */}
         <motion.div
           drag
           dragSnapToOrigin
@@ -263,6 +264,8 @@ const Components: React.FC<Props> = ({
             </g>
           </svg>
         </motion.div>
+
+        {/* Entry Component */}
         <motion.div
           drag
           dragSnapToOrigin
@@ -301,6 +304,8 @@ const Components: React.FC<Props> = ({
             </g>
           </svg>
         </motion.div>
+
+        {/* Divider Component */}
         <motion.div
           drag
           dragSnapToOrigin
@@ -325,14 +330,15 @@ const Components: React.FC<Props> = ({
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth={1.5}
             width="36"
             height="36"
+            preserveAspectRatio="xMidYMid meet"
+            viewBox="0 0 24 24"
           >
-            <path d="M15.75 5.25v13.5m-7.5-13.5v13.5" />
+            <g fill="none" fillRule="evenodd">
+              <path d="M24 0v24H0V0h24ZM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018Zm.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022Zm-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01l-.184-.092Z" />
+              <path fill="currentColor" d="M3 12a1 1 0 0 1 1-1h16a1 1 0 1 1 0 2H4a1 1 0 0 1-1-1Z" />
+            </g>
           </svg>
         </motion.div>
       </div>
