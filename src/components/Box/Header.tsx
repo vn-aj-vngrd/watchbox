57c320610d0b8bbe25c0df09449ab9eebd4d3cb8
teddzyb <teddybartletg88@gmail.com>
2022-12-14 16:55:36 +0800

    Change box header title from input to span and add 'saved' indicator

--- src/components/Box/Header.tsx
+++ src/components/Box/Header.tsx
@@ -1,9 +1,12 @@
 import { Menu, Transition } from "@headlessui/react";
 import { LinkIcon } from "@heroicons/react/20/solid";
-import { HeartIcon as OutlineHeartIcon } from "@heroicons/react/24/outline";
+import {
+  HeartIcon as OutlineHeartIcon,
+  CheckCircleIcon,
+  ArrowPathIcon,
+} from "@heroicons/react/24/outline";
 import {
   HeartIcon as SolidHeartIcon,
-  CheckIcon,
   EllipsisVerticalIcon,
   LockClosedIcon,
   GlobeAsiaAustraliaIcon,
@@ -12,8 +15,7 @@ import {
 import { Box, FavoriteBox, User } from "@prisma/client";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
-import { useEffect, useState, Fragment } from "react";
-import { SubmitHandler, useForm } from "react-hook-form";
+import { useEffect, useState, useRef, Fragment } from "react";
 import toast from "react-hot-toast";
 import { server } from "../../config";
 import { trpc } from "../../utils/trpc";
@@ -28,13 +30,11 @@ type Props = {
   refetch: () => void;
 };
 
-type Inputs = {
-  boxTitle: string;
-};
-
 const Header = ({ box, favoriteBox, id, refetch }: Props) => {
   const { data: session } = useSession();
   const router = useRouter();
+  const spanRef = useRef<HTMLSpanElement>(null);
+  const [errors, setErrors] = useState<string[]>([]);
 
   const addFavoriteBox = trpc.useMutation("favorite.addFavoriteBox", {
     onSuccess: () => {
@@ -64,23 +64,11 @@ const Header = ({ box, favoriteBox, id, refetch }: Props) => {
 
   const [favorite, setFavorite] = useState<boolean>(false);
   const [isPublic, setIsPublic] = useState<boolean>(false);
-  const [isBoxTitleChanged, setIsBoxTitleChanged] = useState<boolean>(false);
-  const {
-    register,
-    handleSubmit,
-    watch,
-    reset,
-    formState: { errors },
-  } = useForm<Inputs>({ mode: "all" });
-
-  const boxTitle_watch = watch("boxTitle");
 
   useEffect(() => {
     setFavorite(favoriteBox !== null && favoriteBox !== undefined ? true : false);
     setIsPublic(box?.boxes[0]?.isPublic ? true : false);
-    setIsBoxTitleChanged(false);
-    reset({ boxTitle: box?.boxes[0]?.boxTitle || "" });
-  }, [favoriteBox, box, reset]);
+  }, [favoriteBox, box]);
 
   const onFavoriteBox = () => {
     if (favorite) {
@@ -119,78 +107,61 @@ const Header = ({ box, favoriteBox, id, refetch }: Props) => {
     });
   };
 
-  const onUpdateBoxTitle: SubmitHandler<Inputs> = async (data) => {
-    setIsBoxTitleChanged(true);
+  const handleBlur = () => {
+    const title = spanRef.current?.innerText;
+
+    if (spanRef.current && title === "") {
+      spanRef.current.innerText = box?.boxes[0]?.boxTitle as string;
+      return;
+    }
+
+    if (title && title.length > 25) {
+      setErrors(["Title cannot exceed 25 characters"]);
+      return;
+    }
+
+    setErrors([]);
+
     updateBox.mutateAsync({
       id,
-      boxTitle: data.boxTitle.charAt(0).toUpperCase() + data.boxTitle.slice(1),
-      isPublic,
+      boxTitle: title as string,
+      isPublic: isPublic,
     });
   };
 
   return (
     <div className="flex h-12 items-center border-b pl-4 pr-2 dark:border-darkColor">
-      {/* TODO: Try onBlur on the input instead of onSubmit for better UX */}
-      <form onSubmit={handleSubmit(onUpdateBoxTitle)} className="flex h-full grow items-center">
-        <input
-          type="text"
-          disabled={session?.user?.id !== box?.id}
-          className="mt-px w-48 border-b border-b-transparent bg-transparent font-medium focus:border-b-gray-200 focus:outline-none hover:border-b-gray-200 dark:focus:border-b-darkColor dark:hover:border-b-darkColor md:w-64"
-          {...register("boxTitle", {
-            required: {
-              value: true,
-              message: "Box title is required",
-            },
-            minLength: {
-              value: 4,
-              message: "Title must be at least 4 characters.",
-            },
-          })}
-        />
-        {errors.boxTitle && errors.boxTitle.message ? (
+      <div className="flex h-full grow items-center">
+        <span
+          ref={spanRef}
+          onBlur={handleBlur}
+          className="mt-px border-b border-b-transparent pl-1 pr-3 outline-none focus:border-b-gray-200 hover:border-b-gray-200 dark:focus:border-b-darkColor dark:hover:border-b-darkColor"
+          spellCheck="false"
+          contentEditable
+          suppressContentEditableWarning
+        >
+          {box?.boxes[0]?.boxTitle}
+        </span>
+        {errors.length > 0 ? (
           <Popover.Root>
             <Popover.Trigger>
-              <InformationCircleIcon className="-ml-4 h-4 w-4 text-red-500" />
+              <InformationCircleIcon className="mt-0.5 mr-2 h-4 w-4 text-red-500" />
             </Popover.Trigger>
             <Popover.Content
               align="center"
               side="bottom"
               sideOffset={4}
-              className="-ml-4 rounded-md bg-red-500 px-3 py-2 text-sm text-white"
+              className="-ml-1.5 rounded-md bg-red-500 px-3 py-2 text-sm text-white"
             >
-              <Popover.Arrow className="ml-4 fill-red-500" />
-              {errors.boxTitle && errors.boxTitle.message}
+              <Popover.Arrow className="ml-1.5 fill-red-500" />
+              {errors[0]}
             </Popover.Content>
           </Popover.Root>
-        ) : null}
-
-        {boxTitle_watch !== box?.boxes[0]?.boxTitle && !errors.boxTitle && (
-          <button className="flex h-full items-center" type="submit" disabled={isBoxTitleChanged}>
-            {isBoxTitleChanged ? (
-              <>
-                <svg
-                  role="status"
-                  className="ml-1 inline-flex h-4 w-4 animate-spin fill-blue-600 text-gray-300 dark:text-gray-600"
-                  viewBox="0 0 100 101"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                    fill="currentColor"
-                  />
-                  <path
-                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                    fill="currentFill"
-                  />
-                </svg>
-              </>
-            ) : (
-              <CheckIcon className="h-6 w-6 text-blue-500" />
-            )}
-          </button>
+        ) : (
+          <CheckCircleIcon className="mt-0.5 mr-2 h-4 w-4 dark:text-green-500" />
+          // <ArrowPathIcon className="w-4 h-4 mt-0.5 mr-2 dark:text-amber-500 animate-pulse" />
         )}
-      </form>
+      </div>
 
       <button
         onClick={onFavoriteBox}
