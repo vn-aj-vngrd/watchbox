b51874477457db5a247f24da3a0be560d138c24e
Heischichou <jangarot@gmail.com>
2022-12-17 17:48:35 +0800

    Add React-Markdown Editor

--- next.config.js
+++ next.config.js
@@ -6,23 +6,29 @@ const withPWA = require("next-pwa")({
   disable: process.env.NODE_ENV === "development",
 });
 
-module.exports = withPWA({
-  // next.js config
-  reactStrictMode: true,
-  swcMinify: true,
+// next-remove-imports
+const removeImports = require("next-remove-imports")({
+  test: /node_modules([\s\S]*?)\.(tsx|ts|js|mjs|jsx)$/,
+  matchImports: "\\.(less|css|scss|sass|styl)$",
 });
 
-module.exports = {
-  images: {
-    domains: [
-      "encrypted-tbn1.gstatic.com",
-      "encrypted-tbn2.gstatic.com",
-      "encrypted-tbn3.gstatic.com",
-      "res.cloudinary.com",
-      "lh3.googleusercontent.com",
-      "pbs.twimg.com",
-      "cdn.discordapp.com",
-      "www.themoviedb.org",
-    ],
-  },
-};
+module.exports = removeImports(
+  withPWA({
+    // next.js config
+    reactStrictMode: true,
+    swcMinify: true,
+    experimental: { esmExternals: true },
+    images: {
+      domains: [
+        "encrypted-tbn1.gstatic.com",
+        "encrypted-tbn2.gstatic.com",
+        "encrypted-tbn3.gstatic.com",
+        "res.cloudinary.com",
+        "lh3.googleusercontent.com",
+        "pbs.twimg.com",
+        "cdn.discordapp.com",
+        "www.themoviedb.org",
+      ],
+    },
+  }),
+);
