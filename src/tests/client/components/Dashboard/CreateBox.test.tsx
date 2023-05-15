6ff9cb02841cde55e8771e1fe0b666d73c39af71
teddzyb <teddybartletg88@gmail.com>
2023-05-15 20:30:13 +0800

    cleanup and fix errors

--- src/tests/client/components/Dashboard/CreateBox.test.tsx
+++ src/tests/client/components/Dashboard/CreateBox.test.tsx
@@ -3,82 +3,80 @@ import CreateBox from "../../../../components/Dashboard/CreateBox";
 import { useForm } from "react-hook-form";
 
 jest.mock("next/router", () => ({
-    useRouter: () => ({
-      push: jest.fn(),
-    }),
-  }));
+  useRouter: () => ({
+    push: jest.fn(),
+  }),
+}));
 
 jest.mock("../../../../utils/trpc", () => ({
-    trpc: {
-        useMutation: jest.fn().mockReturnValue({
-            mutateAsync: jest.fn(),
-        }),
-        isLoading: false,
-    },
+  trpc: {
+    useMutation: jest.fn().mockReturnValue({
+      mutateAsync: jest.fn(),
+    }),
+    isLoading: false,
+  },
 }));
 
 jest.mock("react-hook-form", () => ({
-    useForm: jest.fn().mockReturnValue({
-        handleSubmit: jest.fn(),
-        register: jest.fn(),
-        reset: jest.fn(),
-        formState: {
-            errors: jest.fn(),
-        }
-    })
-}))
+  useForm: jest.fn().mockReturnValue({
+    handleSubmit: jest.fn(),
+    register: jest.fn(),
+    reset: jest.fn(),
+    formState: {
+      errors: jest.fn(),
+    },
+  }),
+}));
 
 describe("CreateBox component", () => {
-    const mockProps = {
-        onBoxCreated: jest.fn(),
-        isFirstBox: true,
-    }
+  const mockProps = {
+    onBoxCreated: jest.fn(),
+    isFirstBox: true,
+  };
 
-    test("should render create box button", () => {
-        render(<CreateBox {...mockProps} />);
-        const buttonElement = screen.getByRole("button", {
-            name: /create your first box/i,
-        });
-        expect(buttonElement).toBeInTheDocument();
+  test("should render create box button", () => {
+    render(<CreateBox {...mockProps} />);
+    const buttonElement = screen.getByRole("button", {
+      name: /create your first box/i,
+    });
+    expect(buttonElement).toBeInTheDocument();
+  });
+
+  test("should open dialog on button click", async () => {
+    render(<CreateBox {...mockProps} />);
+    const buttonElement = screen.getByRole("button", {
+      name: /create your first box/i,
+    });
+    fireEvent.click(buttonElement);
+    const dialogElement = await screen.findByRole("dialog");
+    expect(dialogElement).toBeInTheDocument();
+  });
+
+  test("should submit form on create box button click", async () => {
+    const onBoxCreated = jest.fn();
+
+    render(<CreateBox onBoxCreated={onBoxCreated} isFirstBox={true} />);
+
+    const buttonElement = screen.getByRole("button", {
+      name: /create your first box/i,
     });
 
-    test("should open dialog on button click", async () => {
-        render(<CreateBox {...mockProps} />);
-        const buttonElement = screen.getByRole("button", {
-            name: /create your first box/i,
-        });
-        fireEvent.click(buttonElement);
-        const dialogElement = await screen.findByRole("dialog");
-        expect(dialogElement).toBeInTheDocument();
+    await act(async () => {
+      fireEvent.click(buttonElement);
     });
 
-    test("should submit form on create box button click", async () => {
-        const onBoxCreated = jest.fn();
+    const titleInputElement = screen.getByTestId("title");
 
-        render(<CreateBox
-            onBoxCreated={onBoxCreated}
-            isFirstBox={true} />);
-
-        const buttonElement = screen.getByRole("button", {
-            name: /create your first box/i,
-        });
-
-        await act(async () => {
-            fireEvent.click(buttonElement);
-        })
-
-        const titleInputElement = screen.getByTestId("title");
-
-        act(() => {
-            fireEvent.change(titleInputElement, { target: { value: "Test Box" } });
-        })
-
-        const submitButtonElement = screen.getByTestId("create-button");
-
-        act(() => {
-            fireEvent.click(submitButtonElement);
-        })
-
-        expect(useForm().handleSubmit).toHaveBeenCalled();
+    await act(async () => {
+      fireEvent.change(titleInputElement, { target: { value: "Test Box" } });
     });
+
+    const submitButtonElement = screen.getByTestId("create-button");
+
+    await act(async () => {
+      fireEvent.submit(submitButtonElement);
+    });
+
+    expect(useForm().handleSubmit).toHaveBeenCalled();
+  });
 });
