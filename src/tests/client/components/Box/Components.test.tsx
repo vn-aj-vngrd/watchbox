3fd50da1d687f31922c2c2ea3550f7b37716bcb9
teddzyb <teddybartletg88@gmail.com>
2023-05-15 19:41:20 +0800

    add test for Components

---
+++ src/tests/client/components/Box/Components.test.tsx
@@ -0,0 +1,38 @@
+import { render } from "@testing-library/react";
+import Components from "../../../../components/Box/Components";
+import { RefObject } from "react";
+
+jest.mock("../../../../utils/trpc", () => ({
+  trpc: {
+    useMutation: jest.fn().mockReturnValue({
+      mutateAsync: jest.fn(),
+    }),
+  },
+}));
+
+const canvasRef = { current: { scrollLeft: 0, scrollTop: 0 } } as RefObject<HTMLDivElement>;
+const canvasSizeRef = { current: { scrollWidth: 0, scrollHeight: 0 } } as RefObject<HTMLDivElement>;
+const addStateComponent = jest.fn();
+const updateStateComponent = jest.fn();
+const setTemp = jest.fn();
+const sidePanel = true;
+
+describe("Components", () => {
+  test("renders components", () => {
+    const { getByLabelText } = render(
+      <Components
+        id="1"
+        canvasRef={canvasRef}
+        canvasSizeRef={canvasSizeRef}
+        addStateComponent={addStateComponent}
+        updateStateComponent={updateStateComponent}
+        setTemp={setTemp}
+        sidePanel={sidePanel}
+      />
+    );
+
+    expect(getByLabelText("Text Component")).toBeInTheDocument();
+    expect(getByLabelText("Entry Component")).toBeInTheDocument();
+    expect(getByLabelText("Divider Component")).toBeInTheDocument();
+  });
+});
